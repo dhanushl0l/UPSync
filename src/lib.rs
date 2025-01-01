6a47ddaf9651b7267b122168df39be42b3eedd26
dhanushl0l <dhanushdjtherock@gmail.com>
2025-01-01 20:06:16 +0530

    app can now check the client's status

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod state {
     use battery;
-    use std::io;
+    use std::{io, process::Command};
+
     pub fn battery_present() -> Result<battery::State, battery::Error> {
         let manager = battery::Manager::new()?;
 
@@ -8,10 +9,15 @@ pub mod state {
             Some(Ok(battery)) => battery,
             Some(Err(e)) => return Err(e),
             None => {
-                eprintln!("Unable to find any batteries");
                 return Err(io::Error::from(io::ErrorKind::NotFound).into());
             }
         };
         return Ok(battery.state());
+    }
+
+    pub fn client_state(command: String) -> Result<bool, io::Error> {
+        let output = Command::new("sh").arg("-c").arg(command).output()?;
+
+        Ok(output.status.success())
     }
 }
