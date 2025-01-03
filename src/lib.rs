4d0e69977d31c0414c9f5eba3f818e17e78574ba
dhanushl0l <dhanushdjtherock@gmail.com>
2025-01-03 16:38:45 +0530

    Now the app can initiate an SSH connection to the client

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
 pub mod state {
     use battery;
-    use std::{io, process::Command, thread, time};
+    use std::{fs, io, process::Command, thread, time};
+
+    use ssh2::Session;
+    use std::io::prelude::*;
+    use std::net::TcpStream;
 
     pub struct ClientAction {
         pub command: String,
         pub action: bool,
+    }
+
+    pub struct Ssh {
+        pub user_name: String,
+        pub ip: String,
+        pub key: String,
+        pub command: String,
     }
 
     pub fn battery_present() -> Result<battery::State, battery::Error> {
@@ -35,15 +46,56 @@ pub mod state {
         Ok(output.status.success())
     }
 
+    pub fn read_file_for_testing() -> Ssh {
+        let ssh = fs::read_to_string("ssh.txt").expect("unable to read");
+        let mut ssh_vec: Vec<&str> = Vec::new();
+        for data in ssh.lines() {
+            ssh_vec.push(data);
+        }
+
+        Ssh {
+            user_name: ssh_vec[0].to_string(),
+            ip: ssh_vec[1].to_string(),
+            key: ssh_vec[2].to_string(),
+            command: ssh_vec[3].to_string(),
+        }
+    }
+
+    pub fn exigute_ssh(ssh: Ssh) -> Result<(), Box<dyn std::error::Error>> {
+        // this fn is not optimized yet to do what we expect to to,
+        let tcp = TcpStream::connect(&ssh.ip)?;
+        let mut sess = Session::new().unwrap();
+        sess.set_tcp_stream(tcp);
+        sess.handshake().unwrap();
+
+        sess.userauth_password(&ssh.user_name, &ssh.key)?;
+        assert!(sess.authenticated());
+
+        let mut channel = sess.channel_session()?;
+        channel.exec(&ssh.command)?;
+        let mut s = String::new();
+        channel.read_to_string(&mut s).expect("hell1");
+        println!("{}", s);
+        let _ = channel.wait_close();
+        println!("{}", channel.exit_status()?);
+
+        Ok(())
+    }
+
     pub fn run(command: ClientAction) {
         loop {
-            thread::sleep(time::Duration::from_secs(1));
             match battery_present() {
                 Ok(state) => println!("Battery state: {}", state),
                 Err(e) => eprintln!("Error: {}", e),
             }
 
             println!("{}", clinet_communication(&command).unwrap());
+
+            if command.action {
+                let _ = exigute_ssh(read_file_for_testing());
+            }
+
+            thread::sleep(time::Duration::from_secs(1));
         }
     }
 }
